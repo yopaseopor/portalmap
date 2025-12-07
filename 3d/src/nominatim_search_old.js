e5f8309833f57d1b413eb3d352abd0be64604e4c
yopaseopor <yopaseopor@gmail.com>
2025-12-08 00:23:30 +0100

    nominatim

--- 3d/src/nominatim_search_old.js
+++ 3d/src/nominatim_search_old.js
@@ -28,10 +28,15 @@ function initNominatimSearch(map) {
                     // Show loading state
                     container.addClass('loading');
                     
-                    // Make the Nominatim API call using GET request to avoid preflight CORS
+                    // Make the Nominatim API call
                     $.ajax({
-                        url: 'https://nominatim.openstreetmap.org/search?q=' + encodeURIComponent(query) + '&format=json&limit=10&addressdetails=1',
-                        method: 'GET',
+                        url: 'https://nominatim.openstreetmap.org/search',
+                        data: {
+                            q: query,
+                            format: 'json',
+                            limit: 10,
+                            addressdetails: 1
+                        },
                         success: function(data) {
                             resultsList.empty();
                             if (data && data.length > 0) {
@@ -116,4 +121,4 @@ function initNominatimSearch(map) {
     map.addControl(new ol.control.Control({
         element: searchControlBuild()
     }));
-}
+} 
